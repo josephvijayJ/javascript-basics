aa3ec46f76a3f0f5f4fdd103a1f1b92872f7e03c
josephvijayJ <josephvijayj17bca015@gmail.com>
2022-07-16 12:11:15 +0530

    oops commit

--- Oops.js
+++ Oops.js
@@ -1,19 +1,17 @@
-function Books(type,Author,year){
-
-this.type = type;
-this.Author = Author;
-this.year = year;
-this.getAges = function(){
+function Books(type, Author, year) {
+  this.type = type;
+  this.Author = Author;
+  this.year = year;
+  this.getAges = function () {
     const age = new Date().getFullYear() - year;
     return `this book is ${age} years old`;
+  };
 }
-};
 
+console.log('jose');
 
-
-
-const book1 = new Books('crime','Rajesh',2007);
-const book2= new Books('Romatic','Gangadhar',2011);
-const book3 = new Books('Family-Subject','Kalyani',1995);
+const book1 = new Books('crime', 'Rajesh', 2007);
+const book2 = new Books('Romatic', 'Gangadhar', 2011);
+const book3 = new Books('Family-Subject', 'Kalyani', 1995);
 
 console.log(book3.getAges());
