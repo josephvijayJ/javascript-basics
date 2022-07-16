cba1ea033ab26ccb9230d6f9c3654e8ac8d1fed5
josephvijayJ <josephvijayj17bca015@gmail.com>
2022-07-16 12:34:11 +0530

    in index third commit

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-function map(f,cubefn,a) {
-    let result = []; // Create a new Array
-    let square =[];
-    let i; // Declare variable
-    for (i = 0; i != a.length; i++)
-    
-      result.push(f(a[i]));
-    //   square.push(cubefn(a[i]));
-      return result;
-    
+function map(f, cubefn, a) {
+  let result = []; // Create a new Array
+  let square = [];
+  let i; // Declare variable
+  for (i = 0; i != a.length; i++) result.push(f(a[i]));
+  //   square.push(cubefn(a[i]));
+  return result;
 }
 
-  const f = function(x) {
-     return x * x;
-  }
-  const cubefn = function(x){
-      return x*x*x;
-  }
-  let numbers = [0, 1, 2, 5, 10];
-  let cube = map(f,cubefn,numbers);
-  console.log(cube);
+const f = function (x) {
+  return x * x;
+};
+const cubefn = function (x) {
+  return x * x * x;
+};
+let numbers = [0, 1, 2, 5, 10];
+let cube = map(f, cubefn, numbers);
+console.log(cube);
+console.log('cube index.js changes');
